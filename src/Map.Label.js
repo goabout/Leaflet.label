704918165c4c6534d13e62581335a8cfec3f8452
Artem Golovin <agv.c1d@gmail.com>
2015-08-17 09:54:42 +0300

    Remove labels on touch devices

--- src/Map.Label.js
+++ src/Map.Label.js
@@ -1,5 +1,14 @@
 L.Map.include({
+	shownLabel: null,
+
 	showLabel: function (label) {
+
+		//Remember the last shown label to hide it on touch devices
+		if (shownLabel) {
+			shownLabel.close();
+		}
+		shownLabel = label;
+
 		return this.addLayer(label);
 	}
 });
